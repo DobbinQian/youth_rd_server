09211f6b17cbd59b73e18109639a86021c1ed1bf
384665440 <384665440@qq.com>
2019-11-09 11:00:02 +0800

    删除/getValidCode接口 删除/login接口的参数validCode 修改UserMapper中的错误

--- src/main/java/com/youth_rd/demo/controller/LoginAndRegistration.java
+++ src/main/java/com/youth_rd/demo/controller/LoginAndRegistration.java
@@ -1,6 +1,5 @@
 package com.youth_rd.demo.controller;
 
-import com.google.code.kaptcha.Constants;
 import com.google.code.kaptcha.Producer;
 import com.youth_rd.demo.domain.User;
 import com.youth_rd.demo.service.EmailService;
@@ -9,11 +8,7 @@ import com.youth_rd.demo.tools.ServerResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.imageio.ImageIO;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.awt.image.BufferedImage;
 import java.util.Map;
 
 @RestController
@@ -29,47 +24,41 @@ public class LoginAndRegistration {
     private EmailService emailService;
 
     //获取登录验证码
-    @RequestMapping("/getValidCode")
-    public ServerResponse getValidCode(HttpServletRequest request, HttpServletResponse response) throws Exception{
-        response.setDateHeader("Expires", 0);
-        // Set standard HTTP/1.1 no-cache headers.
-        response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
-        // Set IE extended HTTP/1.1 no-cache headers (use addHeader).
-        response.addHeader("Cache-Control", "post-check=0, pre-check=0");
-        // Set standard HTTP/1.0 no-cache header.
-        response.setHeader("Pragma", "no-cache");
-        response.setContentType("image/jpeg");
-        // create the text for the image
-        String capText = captchaProducer.createText();
-        // store the text in the session
-        request.getSession().setAttribute(Constants.KAPTCHA_SESSION_KEY, capText);
-        // create the image with the text
-        BufferedImage bi = captchaProducer.createImage(capText);
-        ServletOutputStream out = response.getOutputStream();
-        // write the data out
-        ImageIO.write(bi, "jpg", out);
-        try {
-            out.flush();
-        } finally {
-            out.close();
-        }
-        return null;
-    }
+//    @RequestMapping("/getValidCode")
+//    public ServerResponse getValidCode(HttpServletRequest request, HttpServletResponse response) throws Exception{
+//        response.setDateHeader("Expires", 0);
+//        // Set standard HTTP/1.1 no-cache headers.
+//        response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+//        // Set IE extended HTTP/1.1 no-cache headers (use addHeader).
+//        response.addHeader("Cache-Control", "post-check=0, pre-check=0");
+//        // Set standard HTTP/1.0 no-cache header.
+//        response.setHeader("Pragma", "no-cache");
+//        response.setContentType("image/jpeg");
+//        // create the text for the image
+//        String capText = captchaProducer.createText();
+//        // store the text in the session
+//        request.getSession().setAttribute(Constants.KAPTCHA_SESSION_KEY, capText);
+//        // create the image with the text
+//        BufferedImage bi = captchaProducer.createImage(capText);
+//        ServletOutputStream out = response.getOutputStream();
+//        // write the data out
+//        ImageIO.write(bi, "jpg", out);
+//        try {
+//            out.flush();
+//        } finally {
+//            out.close();
+//        }
+//        return null;
+//    }
 
     //登录
     @ResponseBody
     @RequestMapping(value = "/login",method = RequestMethod.POST)
     public ServerResponse login(@RequestBody Map<String,String> jsonObj,
                                 HttpServletRequest request){
-
-        String validCode = jsonObj.get("validCode");
         String email = jsonObj.get("email");
         String pwd = jsonObj.get("pwd");
 
-        String key = (String) request.getSession().getAttribute(Constants.KAPTCHA_SESSION_KEY);
-        if(key!=validCode){
-            return ServerResponse.createByError("验证码错误");
-        }
         User user = userService.getUserByEmailAndPwd(email,pwd);
 
         if(user==null){
@@ -105,13 +94,14 @@ public class LoginAndRegistration {
         String pwd = jsonObj.get("pwd");
         String rePwd = jsonObj.get("rePwd");
 
-        if(!pwd.equals(rePwd)){
-            return ServerResponse.createByError("重复密码不正确");
-        }
         String key = (String) request.getSession().getAttribute("registerCode");
         if(!key.equals(validCode)){
             return ServerResponse.createByError("邮箱验证码不正确");
         }
+        if(!pwd.equals(rePwd)){
+            return ServerResponse.createByError("重复密码不正确");
+        }
+        //TODO 判断密码是否符合要求
         request.getSession().setAttribute("registerCode",null);
         int result = userService.addUserByEmailAndPwd(email,pwd);
         if(result == 0){
