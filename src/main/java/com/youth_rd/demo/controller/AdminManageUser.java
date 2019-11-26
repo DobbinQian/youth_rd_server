1a07cb08781a3dac892b7b45881fba40e645adc6
384665440 <384665440@qq.com>
2019-11-26 13:33:51 +0800

    完成接口/adm/getUserList和/adm/banOrReCover的测试

--- src/main/java/com/youth_rd/demo/controller/AdminManageUser.java
+++ src/main/java/com/youth_rd/demo/controller/AdminManageUser.java
@@ -15,21 +15,54 @@ public class AdminManageUser {
     UserManageService userManageService;
     //获取用户列表信息
     @RequestMapping("/adm/getUserList")
-    public ServerResponse getUserList(@RequestParam("email") String email,
-                                      @RequestParam("name") String name,
-                                      @RequestParam("id") Integer id,
-                                      @RequestParam("curr") Integer curr,
-                                      @RequestParam("limit") Integer limit){
-        List<Map<String,Object>> resultList = userManageService.getAllUser(email, name, id, curr, limit);
-        return ServerResponse.createBySuccess("获取指定用户列表信息成功",resultList);
+    public ServerResponse getUserList(@RequestBody Map<String,String> jsonObj){
+        String id = jsonObj.get("id");
+        String email = jsonObj.get("email");
+        String name = jsonObj.get("name");
+        String curr = jsonObj.get("curr");
+        String limit = jsonObj.get("limit");
+        String errStr = "";
+        if(curr==null){
+            errStr+="curr为null";
+        }
+        if(limit==null){
+            errStr+="limit为null";
+        }
+        if(errStr.length()>0){
+            return ServerResponse.createByError(errStr);
+        }
+        if(email!=null&&email.equals("")){
+            email=null;
+        }
+        if(name!=null&&name.equals("")){
+            name=null;
+        }
+        Integer iId = id==null||id.equals("")?null:Integer.valueOf(id);
+        List<Map<String,Object>> resultList = userManageService.getAllUser(email, name, iId, Integer.valueOf(curr), Integer.valueOf(limit));
+        String number = String.valueOf(resultList.get(0).get("number"));
+        resultList.remove(resultList.get(0));
+        return ServerResponse.createBySuccess(number,resultList);
     }
 
     //封禁/取消封禁
     @RequestMapping(value = "/adm/banOrReCover",method = RequestMethod.POST)
     public ServerResponse banOrReCover(@RequestBody Map<String,String> jsonObj){
-        Integer id = Integer.valueOf(jsonObj.get("id"));
-        Integer op = Integer.valueOf(jsonObj.get("op"));
-        int result = userManageService.banOrRecover(id, op);
+
+
+        String id = jsonObj.get("id");
+        String op = jsonObj.get("op");
+        String errStr = "";
+        if(id==null){
+            errStr+="id为null";
+        }
+        if(op==null){
+            errStr+="op为null";
+        }
+        if(errStr.length()>0){
+            System.out.println("id:"+id+"--"+"op:"+op);
+            return ServerResponse.createByError(errStr);
+        }
+        int result = userManageService.banOrRecover(Integer.valueOf(id), Integer.valueOf(op));
         if(result==0){
             return ServerResponse.createByError("数据库异常，操作失败");
         }
--- src/main/java/com/youth_rd/demo/service/impl/UserManageServiceImpl.java
+++ src/main/java/com/youth_rd/demo/service/impl/UserManageServiceImpl.java
@@ -4,11 +4,11 @@ import com.youth_rd.demo.dao.UserMapper;
 import com.youth_rd.demo.domain.User;
 import com.youth_rd.demo.service.UserManageService;
 import com.youth_rd.demo.tools.PageTool;
-import com.youth_rd.demo.tools.RedisTool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -27,19 +27,26 @@ public class UserManageServiceImpl implements UserManageService {
     public List<Map<String, Object>> getAllUser(String email, String name, Integer id, Integer curr, Integer limit) {
         String key = "allUser_by_"+email+"_"+name+"_"+id;
         List<User> userList;
-        if(RedisTool.hasKey(redisTemplate,key)){
-            userList = RedisTool.getList(redisTemplate,key);
-        }else{
-            userList = userMapper.selectAllByENI(email, name, id);
-            RedisTool.setList(redisTemplate,key,userList);
-        }
+//        if(RedisTool.hasKey(redisTemplate,key)){
+//            userList = RedisTool.getList(redisTemplate,key);
+//        }else{
+//            userList = userMapper.selectAllByENI(email, name, id);
+//            RedisTool.setList(redisTemplate,key,userList);
+//        }
+        userList = userMapper.selectAllByENI(email, name, id);
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         userList = PageTool.getDateByCL(userList,curr,limit);
         List<Map<String,Object>> resultList = new ArrayList<>();
+        Map<String,Object> map1 = new HashMap<>();
+        map1.put("number",userList.size());
+        resultList.add(map1);
         for(User u:userList){
             Map<String,Object> map = new HashMap<>();
             map.put("id",u.getId());
             map.put("name",u.getName());
-            map.put("date",u.getRegisterDate().toString());
+            map.put("email",u.getEmail());
+            map.put("date",formatter.format(u.getRegisterDate()));
+            map.put("isBan",u.getIsBan());
             resultList.add(map);
         }
         return resultList;
@@ -47,7 +54,7 @@ public class UserManageServiceImpl implements UserManageService {
 
     @Override
     public int banOrRecover(Integer id, Integer op) {
-        if(op==1){
+        if(op>0){
             return userMapper.updateIsBanById(id,1);
         }
         return userMapper.updateIsBanById(id,0);
